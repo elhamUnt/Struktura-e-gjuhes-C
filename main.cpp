19b200ea00e791ff167d4dc1154adb1f740fe1d4
elhamUnt <elham.hamiti@unt.edu.mk>
2024-10-18 20:56:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,26 @@
-#include <iostream> // perfshijme lirarine e input/output stream (hyrje/daljet)
+#include <iostream> // Përfshijmë librarinë e input/output stream (hyrje/daljet) për të përdorur cout dhe cin
 
-// Komentet gjithmon shkruhen me //
-// komenti nuk ekzekutohet nga kompajleri
+// Komentet gjithmonë shkruhen me //
+// Komenti nuk ekzekutohet nga kompajleri
 
 /*
-Koment qe eshte ne paragraf
-Edhe ketu eshte koment
+  Ky është një koment që mund të zgjasë në disa rreshta.
+  Këto komente përdoren zakonisht për të përshkruar pjesë më të gjata të kodit.
 */
 
-using namespace std; // Perdorim std:: per te kaluar emertimin e funksionit
+using namespace std; // Përdorim namespace-in 'std' për të shmangur shkrimin e std:: për çdo funksion të standardit (p.sh., std::cout)
 
-int main() // funksioni kryesor
-{          // cdo nje funksion duhet te ket kllape e hapur e madhe
+int main() // Funksioni kryesor i programit, prej të cilit nis ekzekutimi
+{          // Çdo funksion duhet të ketë një kllapë të madhe të hapur për të treguar fillimin e trupit të tij.
 
-  // C++ ka fjal te rezvuara, zakonisht prej IDE jane me ngjyra ()
-  // Ket e shtova prej GithUb
+    // C++ ka fjalë të rezervuara, të cilat zakonisht janë të njohura nga IDE-të dhe kanë ngjyra të veçanta.
+    // Ky koment është shtuar për të treguar se fjalët e rezervuara nuk mund të përdoren si emra variablash.
 
-  cout << "Hello World!\n";
+    // Funksioni 'cout' përdoret për të printuar tekst në ekran (dalje output).
+    // "\n" është një shenjë për rresht të ri (newline).
+    cout << "Hello World!\n"; // Printojmë "Hello World!" dhe kalojmë në rreshtin e ri
 
-} // cdo nje funksion duhet te ket kllape e mbyllur e madhe
+    // Kodi përfundon me kthimin e një vlere nga funksioni main.
+    // Kthimi i 0 tregon se programi është ekzekutuar me sukses.
+    return 0; // Funksioni 'main' duhet të kthejë një vlerë integer, prandaj kthejmë 0 për të treguar sukses
+}
