3055f4e28bb71d3bb2bec00385544388ded7ee7f
elhamUnt <elham.hamiti@unt.edu.mk>
2024-10-18 07:36:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Struktura-e-gjuhes-C/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // perfshijme lirarine e input/output stream (hyrje/daljet)
+
+// Komentet gjithmon shkruhen me //
+// komenti nuk ekzekutohet nga kompajleri
+
+/*
+Koment qe eshte ne paragraf
+Edhe ketu eshte koment
+*/
+
+using namespace std; // Perdorim std:: per te kaluar emertimin e funksionit
+
+int main() // funksioni kryesor
+{          // cdo nje funksion duhet te ket kllape e hapur e madhe
+
+  // C++ ka fjal te rezvuara, zakonisht prej IDE jane me ngjyra ()
+
+  cout << "Hello World!\n";
+
+} // cdo nje funksion duhet te ket kllape e mbyllur e madhe
